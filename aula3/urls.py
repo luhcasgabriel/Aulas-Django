e14c1f7c249f030be92db1ab1f1dda7fb92de2e9
luhcasgabriel <lucastec_adm82@hotmail.com>
2020-02-15 16:04:40 -0300

    [aula-3] Recuperação dados da requisição

--- aula3/urls.py
+++ aula3/urls.py
@@ -1,14 +1,19 @@
 
 from django.urls import path, include
-from .views import index, setacookie, redireciona
+# from .views import index, setacookie, redireciona, show_code, redireciona_http_cats
+from . import views
 app_name = 'aula3'
 
 # nunca import do arquivo 
 
 urlpatterns = [
-    path('', index),
-    path('cookie', setacookie), #cookie criado
-    path('uol', redireciona), #cookie criado
+    path('', views.index),
+    path('cookie', views.setacookie), #cookie criado
+    path('uol', views.redireciona), #cookie criado
+    path('<int:code>', views.show_code),
+    path('cats/<int:code>', views.redireciona_http_cats),
+    path('get/', views.show_get_values),
+    path('post/', views.show_post_values),
 ]
 
 
--- aula3/views.py
+++ aula3/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone # tem que pegar o time zone pq o servidor pode ser com fusohorario diferente
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 
 
@@ -24,6 +25,54 @@ def setacookie(request):
 def redireciona(request):
     return HttpResponseRedirect('http://uol.com.br')
 
+
+def show_code(request, code):
+    html = f'<h1> o código e {code}</h1>'
+    response = HttpResponse(html)
+    return response 
+
+
+
+# https://http.cat/200
+
+
+def redireciona_http_cats(request, code):
+        return HttpResponseRedirect(f'https://http.cat/{code}')
+
+
+def show_get_values(request):
+    # import ipdb; ipdb.set_trace()
+    nome = request.GET.get('nome' , None) # pegando atributos da request
+    if nome is None:
+        html = f'<h1>bem vindo usuário anônimo</h1>'
+    else:
+        html = f'<h1>bem vindo {nome}</h1>'
+    return HttpResponse(html)
+
+@csrf_exempt
+def show_post_values(request):
+    head = ''
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        head = f'<h1>bem vindo {nome} {sobrenome}</h1>'
+
+
+    html = """
+
+
+    <form method="POST">
+
+    <label for="fname">First name:</label><br>
+    <input type="text" id="nome" name="nome" value="John"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="sobrenome" name="sobrenome" value="Doe"><br><br>
+    <input type="submit" value="Enviar">
+    </form> 
+
+    """
+    html_to_reponse = head + html
+    return HttpResponse(html_to_reponse)
 
 
 
